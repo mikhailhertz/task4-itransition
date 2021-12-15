c8dac3ff03913485c57b62d69ef06424fbdbedcb
mikhailhertz <92732643+mikhailhertz@users.noreply.github.com>
2021-12-15 10:06:57 +0300

    Add files via upload

--- Menu.js
+++ Menu.js
@@ -6,13 +6,28 @@ class Menu {
     printOptions() {
         console.log('Available moves:')
         for (var i = 0; i < this.moves.length; ++i) {
-            console.log(i + 1, ' - ', this.moves[i])
+            console.log(i + 1, '-', this.moves[i])
         }
         console.log('0 - exit\n? - help')
     }
     printHelp() {
+        var x = Math.max(...(this.moves.map(e => e.length)))
+        var s = '═'.repeat(x)
+        var j = 1;
+        console.log()
+        console.log('╔' + s + '╦' + s + '╗')
+        console.log('║' + ' '.repeat((x - 1) / 2) + 'W' + ' '.repeat(x / 2) + '║' + ' '.repeat((x - 1) / 2) + 'L' + ' '.repeat(x / 2) + '║')
+        console.log('╠' + s + '╬' + s + '╣')
         this.rules.map.forEach((value, key) => {
-            console.log(key, 'beats', value.join(', '))
+            value.forEach((e, i, a) => {
+                console.log('║' + key + ' '.repeat(x - key.length) + '║' + e + ' '.repeat(x - e.length) + '║')
+                if (j === this.rules.map.size && i === a.length - 1) {
+                    console.log('╚' + s + '╩' + s + '╝')
+                } else {
+                    console.log('╠' + s + '╬' + s + '╣')
+                }
+            })
+            j++;
         })
     }
 }
